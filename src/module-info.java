dd81ce08a56e8a283fefbfa41fc53943a421b9db
Saroj Chalise <chalisesaroj639@gmail.com>
2024-01-30 08:55:27 +0545

    multiplication table

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chali
+ *
+ */
+module helloworldjava {
+}
---
+++ src/myfirstpackage/Multiplication.java
@@ -0,0 +1,15 @@
+package myfirstpackage;
+
+public class Multiplication {
+	public static void main(String[] args) {
+		// i number
+		int i, number;
+		number = 8;
+		for (i = 1; i <= 10; i++) {
+			// 8 X 1 =8
+			System.out.println(number + " X " + i + " =" + number * i);
+		}
+
+	}
+
+}
